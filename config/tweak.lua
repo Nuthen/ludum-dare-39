ceba080ea288a68951fcfee5b1545b421aef1702
Cameron McHenry <camchenry@users.noreply.github.com>
2017-07-31 20:03:44 -0400

    Tweak enemy spawn rate

--- config/tweak.lua
+++ config/tweak.lua
@@ -1,8 +1,8 @@
 -- Using underscores in places where camelCase becomes cumbersome - cmchenry
 return {
-    enemySpawnRate = 1, -- seconds
-    enemy_spawn_rate_increase_per_power_grid = -0.05, -- seconds
-    enemy_spawn_rate_increase_per_turret = -0.05, -- seconds
+    enemySpawnRate = 2, -- seconds
+    enemy_spawn_rate_increase_per_power_grid = -0.1, -- seconds
+    enemy_spawn_rate_increase_per_turret = -0.06, -- seconds
 
     power = 1,
     powerWarningThreshold = 0.3,
@@ -14,8 +14,8 @@ return {
 
     powerDropMultiplier = 0,
 
-    power_drop_increase_for_power_grid_activate = 0.01,
-    power_drop_increase_for_turret_activate = 0.005,
+    power_drop_increase_for_power_grid_activate = 0.009,
+    power_drop_increase_for_turret_activate = 0.004,
 
     power_charged_for_button = .1,
     power_charged_for_flick  = .2,
@@ -48,8 +48,9 @@ return {
     -- keep these to ints
     enemyStage1MinEvolveTime = 5,
     enemyStage1MaxEvolveTime = 10,
-    enemyStage2MinEvolveTime = 10,
-    enemyStage2MaxEvolveTime = 20,
+
+    enemyStage2MinEvolveTime = 25,
+    enemyStage2MaxEvolveTime = 35,
 
     enemyStage1Health = 1,
     enemyStage2Health = 3,
