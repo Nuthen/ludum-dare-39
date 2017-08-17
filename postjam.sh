7e710bf4426bcbcf50385a98bdc9b958ee554861
Cameron McHenry <camchenry@users.noreply.github.com>
2017-08-16 22:54:01 -0400

    Add post jam rename script

---
+++ postjam.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd releases
+mv "Spacesquid Spaceship.love"          "Spacesquid Spaceship (Post-Jam).love"
+mv "Spacesquid Spaceship-macosx.zip"    "Spacesquid Spaceship (Post-Jam)-macosx.zip"
+mv "Spacesquid Spaceship-win32.zip"     "Spacesquid Spaceship (Post-Jam)-win32.zip"
+mv "Spacesquid Spaceship-win64.zip"     "Spacesquid Spaceship-win64 (Post-Jam).zip"
+mv "spacesquid-spaceship-1.0_all.deb"   "spacesquid-spaceship-post-jam-1.0_all.deb"
+cd ..
